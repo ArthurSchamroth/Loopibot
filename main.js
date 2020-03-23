13001df9f8cc8373a50e3625ce97d101574fbe53
Raeskinet <contact@raeskinet.com>
2020-03-23 17:16:50 +0100

    eslint

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
 const client = new Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.reply('pong');
-  if (msg.content === 'everyone') msg.channel.send()
-    
-  
+client.on("message", msg => {
+  if (msg.content === "ping") msg.reply("pong");
+  if (msg.content === "everyone") msg.channel.send();
 });
 
-client.login('NjA2MTYwMjY4NjQ4NjQ0NjMw.XUHAyA.oB-__YWe5RPpJqqq6Q44nXijFnU');
+client.login("NjA2MTYwMjY4NjQ4NjQ0NjMw.Xnjdkw.2D_OoIKjkZcPZVJ7pEdBMDXKDek");
