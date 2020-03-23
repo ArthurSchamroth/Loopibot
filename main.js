2c1b5ebd7b880892c6483f198d3528b22bbbaf2e
Raeskinet <contact@raeskinet.com>
2020-03-23 17:00:28 +0100

    ajout fonctionnalité

---
+++ main.js
@@ -0,0 +1,15 @@
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') msg.reply('pong');
+  if (msg.content === 'everyone') msg.channel.send()
+    
+  
+});
+
+client.login('NjA2MTYwMjY4NjQ4NjQ0NjMw.XUHAyA.oB-__YWe5RPpJqqq6Q44nXijFnU');
