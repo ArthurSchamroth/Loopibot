c9863867f8f80928c5168cecfbe55fe6f9fcef62
Raeskinet <contact@raeskinet.com>
2020-04-17 13:52:15 +0200

    insert insensible à la casse

--- commands/set-db.js
+++ commands/set-db.js
@@ -1,18 +1,32 @@
 exports.run = (client, message) => {
-  
   client.mysql = require("../util/db.js");
-  
-  client.mysql.querySql("delete from userinfo");
 
-  message.guild.members.cache.each(member => {
-    try {
-      const sql = `INSERT INTO userinfo(discordId, pseudo) VALUES(${member.user.id},"${member.user.username}")`;
-      client.mysql.querySql(sql);
-    } catch (e) {
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    }
-  });
+  client.mysql.querySql("truncate userinfo");
+  try {
+    message.guild.members.cache.each((member) => {
+      let name = member.user.username;
+      let ok = false;
+      const pos = [];
+      const str = "'";
+      for (let i = 0; i < name.length; i++) {
+        if (name[i] === str) {
+          pos.push(i);
+          ok = true;
+        }
+      }
+      if (ok === true) {
+        for (let i = 0; i < pos.length; i++) {
+          pos[i]++;
+          name = [name.slice(0, pos[i]), str, name.slice(pos[i])].join('');
+        }
+      }
+      client.mysql.querySql(`INSERT INTO userinfo(discordId, pseudo) VALUES(${member.user.id},'${name}')`);
+
+    });
+  } catch (e) {
+    console.log("erreur : " + e);
+  }
 };
 exports.help = {
-  name: "client"
+  name: "client",
 };
